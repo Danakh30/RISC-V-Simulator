2a04edff77b2382256b15002f2708a422afd99cb
MhamadKhalil <mhamed_khalil@aucegypt.edu>
2023-11-24 19:46:23 +0200

    finished working on parsing

--- parsing.cpp
+++ parsing.cpp
@@ -38,21 +38,20 @@ vector<string> removeSubstring(string& originalString, const string& substringTo
     return tokens;
 }
 
-void parseLUI(string LUI)
+void parseGroup1(string g1) //Parsing LUI and AUIPC
 {
     instruction temp;
-    vector<string> tokens = removeSubstring(LUI, "LUI");
-    temp.opcode = "LUI";
-    temp.rd=stoi(tokens[0].substr(1));
-    temp.immidiate=stoi(tokens[1]);
-    program.push_back(temp);
-}
-
-void parseAUIPC(string AUIPC)
-{
-    instruction temp;
-    vector<string> tokens = removeSubstring(AUIPC, "AUIPC");
-    temp.opcode = "AUIPC";
+    vector<string> tokens;
+    if(g1[0] == 'L')
+    {
+        tokens = removeSubstring(g1, "LUI");
+        temp.opcode = "LUI";
+    }
+    else
+    {
+        tokens = removeSubstring(g1, "AUIPC");
+        temp.opcode = "AUIPC";        
+    }
     temp.rd=stoi(tokens[0].substr(1));
     temp.immidiate=stoi(tokens[1]);
     program.push_back(temp);
@@ -68,120 +67,235 @@ void parseJAL(string JAL)
     program.push_back(temp);
 }
 
-void parseJALR(string JALR)
+void parseGroup2(string g2) //JALR - ADDI - SLTI - SLTIU - XORI - ORI - ANDI - SLLI - SRLI - SRAI
 {
     instruction temp;
-    vector<string> tokens = removeSubstring(JALR, "JALR");
-    temp.opcode = "JALR";
+    vector<string> tokens;
+    if(g2.find("JALR") != string::npos)
+    {
+        tokens = removeSubstring(g2, "JALR");
+        temp.opcode = "JALR";
+    }
+    else if(g2.find("ADDI") != string::npos)
+    {
+        tokens = removeSubstring(g2, "ADDI");
+        temp.opcode = "ADDI";       
+    }
+    else if(g2.find("SLTIU") != string::npos)
+    {
+        tokens = removeSubstring(g2, "SLTIU");
+        temp.opcode = "SLTIU";       
+    }
+    else if(g2.find("SLTI") != string::npos)
+    {
+        tokens = removeSubstring(g2, "SLTI");
+        temp.opcode = "SLTI";       
+    }
+    else if(g2.find("XORI") != string::npos)
+    {
+        tokens = removeSubstring(g2, "XORI");
+        temp.opcode = "XORI";       
+    }
+    else if(g2.find("ORI") != string::npos)
+    {
+        tokens = removeSubstring(g2, "ORI");
+        temp.opcode = "ORI";       
+    }
+    else if(g2.find("ANDI") != string::npos)
+    {
+        tokens = removeSubstring(g2, "ANDI");
+        temp.opcode = "ANDI";       
+    }
+    else if(g2.find("SLLI") != string::npos)
+    {
+        tokens = removeSubstring(g2, "SLLI");
+        temp.opcode = "SLLI";       
+    }
+    else if(g2.find("SRLI") != string::npos)
+    {
+        tokens = removeSubstring(g2, "SRLI");
+        temp.opcode = "SRLI";       
+    }
+    else if(g2.find("SRAI") != string::npos)
+    {
+        tokens = removeSubstring(g2, "SRAI");
+        temp.opcode = "SRAI";       
+    }
     temp.rd=stoi(tokens[0].substr(1));
     temp.r1=stoi(tokens[1].substr(1));
     temp.immidiate=stoi(tokens[2]);
     program.push_back(temp);
 }
 
-void parseBEQ(string BEQ)
+void parseGroup3(string g3) //LB - LH - LW - LBU - LHU
 {
     instruction temp;
-    vector<string> tokens = removeSubstring(BEQ, "BEQ");
-    temp.opcode = "BEQ";
-    temp.r1=stoi(tokens[0].substr(1));
-    temp.r2=stoi(tokens[1].substr(1));
-    temp.immidiate=stoi(tokens[2]);
-    program.push_back(temp); 
-}
-
-void parseBNE(string BNE)
-{
-    instruction temp;
-    vector<string> tokens = removeSubstring(BNE, "BNE");
-    temp.opcode = "BNE";
-    temp.r1=stoi(tokens[0].substr(1));
-    temp.r2=stoi(tokens[1].substr(1));
-    temp.immidiate=stoi(tokens[2]);
-    program.push_back(temp); 
-}
-
-void parseBLT(string BLT)
-{
-    instruction temp;
-    vector<string> tokens = removeSubstring(BLT, "BLT");
-    temp.opcode = "BLT";
-    temp.r1=stoi(tokens[0].substr(1));
-    temp.r2=stoi(tokens[1].substr(1));
-    temp.immidiate=stoi(tokens[2]);
-    program.push_back(temp); 
-}
-
-void parseBGE(string BGE)
-{
-    instruction temp;
-    vector<string> tokens = removeSubstring(BGE, "BGE");
-    temp.opcode = "BGE";
-    temp.r1=stoi(tokens[0].substr(1));
-    temp.r2=stoi(tokens[1].substr(1));
-    temp.immidiate=stoi(tokens[2]);
-    program.push_back(temp); 
-}
-
-void parseBLTU(string BLTU)
-{
-    instruction temp;
-    vector<string> tokens = removeSubstring(BLTU, "BLTU");
-    temp.opcode = "BLTU";
-    temp.r1=stoi(tokens[0].substr(1));
-    temp.r2=stoi(tokens[1].substr(1));
-    temp.immidiate=stoi(tokens[2]);
+    vector<string> tokens;
+    if(g3.find("LBU") != string::npos)
+    {
+        tokens = removeSubstring(g3, "LBU");
+        temp.opcode = "LBU";
+    }
+    else if(g3.find("LHU") != string::npos)
+    {
+        tokens = removeSubstring(g3, "LHU");
+        temp.opcode = "LHU";       
+    }
+    else if(g3.find("LW") != string::npos)
+    {
+        tokens = removeSubstring(g3, "LW");
+        temp.opcode = "LW";       
+    }
+    else if(g3.find("LB") != string::npos)
+    {
+        tokens = removeSubstring(g3, "LB");
+        temp.opcode = "LB";       
+    }
+    else 
+    {
+        tokens = removeSubstring(g3, "LH");
+        temp.opcode = "LH";       
+    }
+    temp.rd = stoi(tokens[0].substr(1));
+    size_t pos = tokens[1].find('(');
+    size_t pos2= tokens[1].find(')');
+    temp.immidiate = stoi(tokens[1].substr(0,pos));
+    temp.r1 = stoi(tokens[1].substr(pos+2,pos2));
     program.push_back(temp);   
 }
 
-void parseBGEU(string BGEU)
+void parseGroup4(string g4) //BEQ - BNE - BLT - BGE - BLTU - BGEU
 {
     instruction temp;
-    vector<string> tokens = removeSubstring(BGEU, "BGEU");
-    temp.opcode = "BGEU";
+    vector<string> tokens;
+    if(g4.find("BEQ") != string::npos)
+    {
+        tokens = removeSubstring(g4, "BEQ");
+        temp.opcode = "BEQ";
+    }
+    else if(g4.find("BNE") != string::npos)
+    {
+        tokens = removeSubstring(g4, "BNE");
+        temp.opcode = "BNE";       
+    }
+    else if(g4.find("BLTU") != string::npos)
+    {
+        tokens = removeSubstring(g4, "BLTU");
+        temp.opcode = "BLTU";       
+    }
+    else if(g4.find("BLT") != string::npos)
+    {
+        tokens = removeSubstring(g4, "BLT");
+        temp.opcode = "BLT";       
+    }
+    else if(g4.find("BGEU") != string::npos)
+    {
+        tokens = removeSubstring(g4, "BGEU");
+        temp.opcode = "BGEU";       
+    }
+
+    else 
+    {
+        tokens = removeSubstring(g4, "BGE");
+        temp.opcode = "BGE";       
+    }
     temp.r1=stoi(tokens[0].substr(1));
     temp.r2=stoi(tokens[1].substr(1));
     temp.immidiate=stoi(tokens[2]);
-    program.push_back(temp);   
+    program.push_back(temp); 
 }
 
-void parseLB(string LB)
+void parseGroup5(string g5) //SB - SH - SW
 {
     instruction temp;
-    vector<string> tokens = removeSubstring(LB, "LB");
-    temp.opcode = "LB";
-    temp.rd = stoi(tokens[0].substr(1));
+    vector<string> tokens;
+    if(g5.find("SB") != string::npos)
+    {
+        tokens = removeSubstring(g5, "SB");
+        temp.opcode = "SB";
+    }
+    else if(g5.find("SH") != string::npos)
+    {
+        tokens = removeSubstring(g5, "SH");
+        temp.opcode = "SH";       
+    }
+    else 
+    {
+        tokens = removeSubstring(g5, "SW");
+        temp.opcode = "SW";       
+    }
+    temp.r2 = stoi(tokens[0].substr(1));
     size_t pos = tokens[1].find('(');
     size_t pos2= tokens[1].find(')');
     temp.immidiate = stoi(tokens[1].substr(0,pos));
     temp.r1 = stoi(tokens[1].substr(pos+2,pos2));
-    program.push_back(temp);
+    program.push_back(temp);  
 }
 
-void parseLH(string LH)
+void parseGroup6(string g6) //ADD - SUB - SLL - SLT - SLTU - XOR - SRL - SRA - OR - AND 
 {
     instruction temp;
-    vector<string> tokens = removeSubstring(LH, "LH");
-    temp.opcode = "LH";
-    temp.rd = stoi(tokens[0].substr(1));
-    size_t pos = tokens[1].find('(');
-    size_t pos2= tokens[1].find(')');
-    temp.immidiate = stoi(tokens[1].substr(0,pos));
-    temp.r1 = stoi(tokens[1].substr(pos+2,pos2));
-    program.push_back(temp);
-}
+    vector<string> tokens;
+    if(g6.find("ADD") != string::npos)
+    {
+        tokens = removeSubstring(g6, "ADD");
+        temp.opcode = "ADD";
+    }
+    else if(g6.find("SUB") != string::npos)
+    {
+        tokens = removeSubstring(g6, "SUB");
+        temp.opcode = "SUB";       
+    }
+    else if(g6.find("SLL") != string::npos)
+    {
+        tokens = removeSubstring(g6, "SLL");
+        temp.opcode = "SLL";       
+    }
+    else if(g6.find("SLTU") != string::npos)
+    {
+        tokens = removeSubstring(g6, "SLTU");
+        temp.opcode = "SLTU";       
+    }
+    else if(g6.find("SLT") != string::npos)
+    {
+        tokens = removeSubstring(g6, "SLT");
+        temp.opcode = "SLT";       
+    }
+    else if(g6.find("XOR") != string::npos)
+    {
+        tokens = removeSubstring(g6, "XOR");
+        temp.opcode = "XOR";       
+    }
+    else if(g6.find("SRL") != string::npos)
+    {
+        tokens = removeSubstring(g6, "SRL");
+        temp.opcode = "SRL";       
+    }
+    else if(g6.find("SRA") != string::npos)
+    {
+        tokens = removeSubstring(g6, "SRA");
+        temp.opcode = "SRA";       
+    }
+    else if(g6.find("OR") != string::npos)
+    {
+        tokens = removeSubstring(g6, "OR");
+        temp.opcode = "OR";       
+    }
+    else 
+    {
+        tokens = removeSubstring(g6, "AND");
+        temp.opcode = "AND";       
+    }
+    temp.rd=stoi(tokens[0].substr(1));
+    temp.r1=stoi(tokens[1].substr(1));
+    temp.r2=stoi(tokens[2].substr(1));
+    program.push_back(temp); 
+} 
 
-void parseLW(string LW)
+void parseGroup7(string g7) //FENCE - ECALL - EBREAK
 {
     instruction temp;
-    vector<string> tokens = removeSubstring(LW, "LW");
-    temp.opcode = "LW";
-    temp.rd = stoi(tokens[0].substr(1));
-    size_t pos = tokens[1].find('(');
-    size_t pos2= tokens[1].find(')');
-    temp.immidiate = stoi(tokens[1].substr(0,pos));
-    temp.r1 = stoi(tokens[1].substr(pos+2,pos2));
-    program.push_back(temp);
+    temp.opcode="FENCE";
 }
 
 void parse(string inst)
@@ -190,17 +304,6 @@ void parse(string inst)
     string temp = inst.substr(0,space);
     FunctionPointer func = parseFunctions[temp];
     func(inst);
-    // else if (temp == "LBU"){}
-    // else if (temp == "LHU"){}
-    // else if (temp == "SB"){}
-    // else if (temp == "SH"){}
-    // else if (temp == "SW"){}
-    // else if (temp == "ADDI"){}
-    // else if (temp == "SLTI"){}
-    // else if (temp == "SLTIU"){}
-    // else if (temp == "XORI"){}
-    // else if (temp == "ORI"){}
-    // else if (temp == "BLT"){}
 }
 
 void extractCode()
